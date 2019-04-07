3b0749d4945ea8e11524f2ea9f0179abda31270b
Ronaldo <ronaldoniz@gmail.com>
2019-04-07 12:34:33 -0300

    commit inicial telas de visualizar/editar evento

---
+++ web/js/eventosVisualizar.js
@@ -0,0 +1,14 @@
+$('#editaNomeEvento').click(function() {
+	$('#divForm').show('fast')
+	$('#divNome').hide('fast')
+})
+
+$('#salvaNomeEvento').click(function() {
+  $('#formNome').submit(function() {
+    return false;
+  })
+	$('#nomeEvento').text($('#inputNome').val())
+	$('#divForm').hide('fast')
+  $('#divNome').show('fast')
+  $('#editaNomeEvento').show('fast')
+})
