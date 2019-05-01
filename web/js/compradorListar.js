b0d6e2700ab51bf12e6dc8d9ab462e3f7e4332e6
Ronaldo <ronaldoniz@gmail.com>
2019-05-01 17:40:24 -0300

    commit inicial lista de compradores

---
+++ web/js/compradorListar.js
@@ -0,0 +1,14 @@
+// $('#toggle').click(function() {
+// 	$('#divForm').show('fast')
+// 	$('#divNome').hide('fast')
+// })
+
+// $('#salvaNomeEvento').click(function() {
+//   $('#formNome').submit(function() {
+//     return false;
+//   })
+// 	$('#nomeEvento').text($('#inputNome').val())
+// 	$('#divForm').hide('fast')
+//   $('#divNome').show('fast')
+//   $('#editaNomeEvento').show('fast')
+// })
