b9abfb384256d0357876c5158fdb4c88e329d352
mateus-picoloto <mateus.picoloto97@gmail.com>
2019-05-11 22:08:44 -0300

    correção sql

--- SQL/Ingresso.sql
+++ SQL/Ingresso.sql
@@ -9,11 +9,11 @@
  */
 
 CREATE TABLE tb_ingresso (
-    id_ingresso INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id_ingresso INT SERIAL PRIMARY KEY,
     serial_ingresso CHAR(10),
-    id_evento INT(6) UNSIGNED,
-    id_usuario INT(6) UNSIGNED,
-    FOREIGN KEY (id_evento) REFERENCES tb_evento(id_evento),
-    FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario)
+    id_lote INT,
+    id_usuario INT,
+    CONSTRAINT fk_ingresso_lote FOREIGN KEY (id_lote) REFERENCES tb_lote(id_lote),
+    CONSTRAINT fk_ingresso_usuario FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario)
 );
 
